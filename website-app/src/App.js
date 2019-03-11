40df82803eec756171fdabe2753592b19b4648ff
bob <bob123edwards123@gmail.com>
2019-03-11 22:05:54 +1100

    updated the website

---
+++ website-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Container, Header} from 'semantic-ui-react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Google
+      </div>
+    );
+  }
+}
+
+export default App;
